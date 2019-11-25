27d7b92420ab61600a426a0b229a45df5d6bdf6c
Fattoh Alqershi <fattoh@gmail.com>
2019-11-25 11:40:32 +0300

    update settings again
    backward of static_root

--- StatementsRate/settings.py
+++ StatementsRate/settings.py
@@ -110,6 +110,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
-#old STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
-#new
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+#alternative STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
